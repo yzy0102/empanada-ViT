3db55a4cea4d9970bf2f25d2f3452c83cd3d9ca6
yzy0102 <54012483+yzy0102@users.noreply.github.com>
2025-02-19 17:56:34 +0800

    Add files via upload
    add panoptic_deeplab_4_ViT.py for ViT backbone

--- empanada/__init__.py
+++ empanada/__init__.py
@@ -1,1 +1,4 @@
-__version__ = '0.1.2'
+from empanada.models.panoptic_deeplab import PanopticDeepLab, PanopticDeepLabPR,  PanopticDeepLabBC
+
+from empanada.models.panoptic_deeplab_4_ViT import PanopticDeepLab_4_ViT,  PanopticDeepLabPR_4_ViT, PanopticDeepLabBC_4_ViT
+from empanada.models.panoptic_bifpn import PanopticBiFPN, PanopticBiFPNPR
